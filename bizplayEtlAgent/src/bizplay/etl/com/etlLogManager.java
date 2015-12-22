bc2f1c067356378136d11e60c425220e71eea92b
baechanjung <qockswnd@naver.com>
2015-12-22 16:22:52 +0900

    logback

--- bizplayEtlAgent/src/bizplay/etl/com/etlLogManager.java
+++ bizplayEtlAgent/src/bizplay/etl/com/etlLogManager.java
@@ -1,20 +1,30 @@
 package bizplay.etl.com;
 
+import java.io.PrintStream;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.classic.joran.JoranConfigurator;
-import ch.qos.logback.core.joran.spi.JoranException;
-import ch.qos.logback.core.util.StatusPrinter;
-
 public class etlLogManager {
+	private static final Logger LOGGER = LoggerFactory.getLogger("");
+
 	public static void etlLog(String logLevel, String errMsg){
-
-		Logger LOGGER = LoggerFactory.getLogger("");
-
+		
+		/*
 		LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
 		
+		System.out.println(System.getProperties().getProperty("logback.configurationFile"));
+
+		try {
+			JoranConfigurator configurator = new JoranConfigurator();
+			configurator.setContext(context);
+			context.reset(); 
+			configurator.doConfigure(System.getProperties().getProperty("logback.configurationFile"));
+		} catch (JoranException je) {
+			// StatusPrinter will handle this
+		}
+		*/
+
 		if("INFO".equals(logLevel)){
 			LOGGER.info(errMsg);
 		}else if("TRACE".equals(logLevel)){
@@ -29,6 +39,32 @@ public class etlLogManager {
 			LOGGER.debug(errMsg);
 		}
 
+	}
+	
+	public static void etlLog(String logLevel, String errMsg, Exception ex){
+		LOGGER.error(errMsg,ex);
+	}
+	
+	public static PrintStream createLoggingSystem(final PrintStream realPrintStream) {
+	    return new PrintStream(realPrintStream) {
+	        public void print(final String string) {
+	        	LOGGER.debug(string);
+	        }
+	        public void println(final String string) {
+	        	LOGGER.debug(string);
+	        }
+	    };
+	}
+	
+	public static PrintStream createLoggingError(final PrintStream realPrintStream) {
+		return new PrintStream(realPrintStream) {
+			public void print(final String string) {
+				LOGGER.error(string);
+			}
+			public void println(final String string) {
+				LOGGER.error(string);
+			}
+		};
 	}
 
 }
